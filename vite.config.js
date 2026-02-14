0a083483257b4294b884bb4225addafc0d8b1851
yonox270 <miguelpereira@yonox-contact.store>
2026-02-14 22:23:22 +0100

    Fix vite alias for shopify.server

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [
@@ -7,4 +11,12 @@ export default defineConfig({
       ignoredRouteFiles: ["**/.*"],
     }),
   ],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./app"),
+    },
+  },
+  ssr: {
+    noExternal: ["@shopify/shopify-app-remix"],
+  },
 });
